fc2207c2c7e41ace867bb93a1bb158e84ff13325
SMB開発部 <sorimachi-aws@hotmail.com>
2014-11-19 17:50:07 +0900

    フォント一覧表示

--- chapter8-3(2nd)/ViewController.swift
+++ chapter8-3(2nd)/ViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UITableViewDataSource, UITableViewDelegate{
 
+    var fontName_array:[String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        for fontFamilyName in UIFont.familyNames() {
+            for fontName in UIFont.fontNamesForFamilyName(fontFamilyName as String) {
+                fontName_array.append(fontName as String)
+            }
+        }
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fontName_array.count
     }
 
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = UITableViewCell(style: .Subtitle, reuseIdentifier: "myCell")
+        var fontName = fontName_array[indexPath.row]
+        cell.textLabel.text = "ABCD abcd 012345 あいうえお"
+        cell.textLabel.font = UIFont(name: fontName, size: 18)
+        
+        cell.detailTextLabel?.textColor = UIColor.brownColor()
+        cell.detailTextLabel?.text = fontName
+        return cell
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
